951f59a9ce29548cf83f3da4155993c32868a063
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-08-31 08:27:09 -0300

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: [process.env.CORS_ORIGIN],
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static('public'));
+
+app.get(
+    "/", (req, res) => {
+      res.status(200).json({
+          test: true
+      })
+    }
+);
+
+app.listen(process.env.PORT, () => {
+    console.log("Servidor escutando na porta 3030...")
+});
+
